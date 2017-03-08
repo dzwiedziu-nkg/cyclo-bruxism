6c7d4eee94f6b9a9b4b94a35f5157e05d65c997a
Bylus <bylus@onet.eu>
2017-03-08 11:32:53 +0100

    Brakujący plik

--- apk/app/src/main/java/pl/nkg/brq/android/ConstValues.java
+++ apk/app/src/main/java/pl/nkg/brq/android/ConstValues.java
@@ -7,7 +7,7 @@ import android.graphics.Color;
  */
 
 public class ConstValues {
-    public static final String BASE_URL = "http://192.168.222.186:8000";
+    public static final String BASE_URL = "http://192.168.56.1:8000";
     public static final int CONNECTION_TIMEOUT = 10000;
     public static final int DATA_CHUNK_SIZE = 20;
 
